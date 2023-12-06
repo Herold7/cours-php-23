93db674da6948f53d65e4d75a7a7468906b1f4a2
Herold7 <makpier@gmail.com>
2023-12-06 09:57:38 +0100

    🔥

--- index.php
+++ index.php
@@ -1,56 +1,19 @@
-<?php
-$age = 32;
-$amount = 19599.50;
-$name = 'Martin';
-$isClient = true;
-$options = ['livraison en 24h', 'paiement en 3 fois'];
+<?php 
+require_once 'data/Data.php';
+include 'partials/header.php'; 
 
-echo $name . ' a ' . $age . ' ans. <br>';
-print($name .= $age);
-print($options[1]);
-var_dump($age);
-var_dump($age);
+// Boucle sur les posts
+foreach ($posts as $item) {
+    // Assignation des variables
+    $id = $item['id'];
+    $title = $item['title'];
+    $content = $item['content'];
+    $author = $users[$item['user_id']]['username'];
+    $commentCount = count($item['comments']);
 
-if (date('H') < 12) {
-    echo 'Bonjour';
-} elseif (date('H') < 18) {
-    echo 'Bon après-midi';
-} else {
-    echo 'Bonsoir';
+    // Inclusion du template
+    include 'partials/post.php';
+    echo '<hr/>';
 }
 
-echo date('jS');
-
-$options = ['livraison en 24h', 'paiement en 3 fois'];
-
-echo '<ul>';
-foreach ($options as $option) {
-    echo '<li>' . $option . '</li>';
-}
-echo '</ul>';
-
-?>
-
-<head>
-    <html>
-
-<body>
-    <form action="signup.php" method="post">
-        <fieldset>
-            <legend>Inscription</legend>
-            <input type="text" name="name" placeholder="Martin" required>
-        </fieldset>
-        <fieldset>
-            <legend>Votre âge</legend>
-            <input type="number" name="age" placeholder="32" required>
-        </fieldset>
-        <fieldset>
-            <legend>Votre 1er dépôt</legend>
-            <input type="number" name="amount" placeholder="1900.50" required>
-        </fieldset>
-        <input type="submit" value="S'inscrire">
-    </form>
-</body>
-
-</html>
-</head>
+include 'partials/footer.php'; ?>
---
+++ partials/post.php
@@ -0,0 +1,20 @@
+<article>
+    <header>
+        <a href="./article.php?id=<?= $id-1; ?>">
+            <h2><?= $title; ?></h2>
+        </a>
+    </header>
+    <p>
+        <?= substr($content, 0, 100) . '...'; ?>
+    </p>
+    <footer>
+        <nav>
+            <ul>
+                <li>Publié par <strong><?= $author; ?></strong></li>
+            </ul>
+            <ul>
+                <li><?= $commentCount; ?> commentaires</li>
+            </ul>
+        </nav>
+    </footer>
+</article>
